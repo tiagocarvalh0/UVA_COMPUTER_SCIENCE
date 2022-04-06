79e91d46798b5f11806fcceeeb854e7a8c613d85
tiagocarvalh0 <tiagocrv501@gmail.com>
2022-04-06 19:42:54 -0300

    up

---
+++ 1º_SEMESTRE/L_PROGRAMACAO/ex001/Nova pasta/text1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	float a, b ,x;
+	printf("Digite um numero: ");
+	scanf("%f", &a);
+	printf("Digite outro numero: ");
+	scanf("%f", &b);
+	x = a+b ;
+	printf("%f \n", x);
+}
