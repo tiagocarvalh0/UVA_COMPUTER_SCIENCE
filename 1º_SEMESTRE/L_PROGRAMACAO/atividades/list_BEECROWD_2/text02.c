3444bf2223d6e379f421bdf7eb665ebb1407b5d8
tiagocarvalh0 <tiagocrv501@gmail.com>
2022-04-14 22:56:22 -0300

    up

--- 1º_SEMESTRE/L_PROGRAMACAO/atividades/list_BEECROWD_2/text02.c
+++ 1º_SEMESTRE/L_PROGRAMACAO/atividades/list_BEECROWD_2/text02.c
@@ -1,35 +1,33 @@
-/*Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".*/
-
 #include <stdio.h>
- 
-int main() {
- 
-	double N1, N2,  N3,  N4, Media, nt ;
 
-	scanf("%lf %lf %lf %lf", &N1, &N2, &N3, &N4);
-	Media =((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10 ;
-	printf("Media: %.1lf\n", Media);
-	
-	if(Media >= 7.0){
-	printf("Aluno reprovado.\n");
+int main(){
+
+    	float n1, n2, n3, n4, x, media, media_f;
+
+    	scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
+    	media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1))/10;
+    	printf("Media: %.1lf\n", media);
+
+    	if(media > 7.0){
+    	printf("Aluno aprovado.\n");
 }
-	if(Media < 5.0){
-	printf("Aluno reprovado.\n");	
+    	if(media < 5.0){
+        printf("Aluno reprovado.\n");
+
 }
-	if((Media >= 5.0) && (Media <= 6.9)){
-	printf("Aluno em exame.\n");
-	scanf("%lf", &nt);
-	printf("Nota do exame: %.1lf\n", nt);
-	Media = (nt + Media) / 2;
-		
-	if(Media >= 5.0){
-	printf("Aluno aprovado.\n");
-	printf("Media final: %.1lf\n", Media);
+    	if(media >= 5.0 && media <=6.9){
+        printf("Aluno em exame.\n");
+        scanf("%f", &x);
+        media_f = (media + x )/2;
+        printf("Nota do exame: %.1f\n", x);
+
+        if(media_f >= 5.0){
+        printf("Aluno aprovado.\n");
+        printf("Media final: %.1f\n", media_f);
 }
-	if(Media <= 4.9){
-	printf("Aluno reprovado.\n");
-	printf("Media final: %.1lf\n", Media);
-}	
+        if(media_f <= 4.9){
+        printf("Aluno reprovado.\n");
+        printf("Media final: %.1f", media_f);
 }
-    return 0;
+}
 }
---
+++ 1º_SEMESTRE/L_PROGRAMACAO/atividades/list_BEECROWD_2/text03.c
@@ -0,0 +1,83 @@
+/*Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).*/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+	float x, y ;
+	
+	scanf("%f %f", &x, &y);
+	
+	if((x == 0.0) && (y == 0.0)){
+	printf("Origem\n");
+}
+
+	else{
+	if(x == 0.0){
+	printf("Eixo Y\n");
+}
+}
+	else{
+	if(y == 0.0){
+	printf("Eixo X\n");
+
+}
+}
+	else{
+	printf("ola");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
