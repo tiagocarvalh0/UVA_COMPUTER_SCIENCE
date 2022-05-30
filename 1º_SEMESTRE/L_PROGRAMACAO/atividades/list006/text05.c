b6f1ca617cc875a0b12d8f0af022c1fd60e39b20
tiagocarvalh0 <tiagocrv501@gmail.com>
2022-05-30 14:44:56 -0300

    up

--- 1º_SEMESTRE/L_PROGRAMACAO/atividades/list006/text05.c
+++ 1º_SEMESTRE/L_PROGRAMACAO/atividades/list006/text05.c
@@ -21,5 +21,19 @@ int main()
 		printf("\n");
 	}
 	
+	for(linha = 0; linha < 20; linha++)
+	{
+		for(coluna = 0; coluna < 20; coluna++)
+		{
+			if(coluna >= linha)
+				printf("-   ");
+			else
+			{
+				printf("%d ", vet[linha][coluna]);
+			}
+		}
+		printf("\n");
+	}
+	
 	return 0;
 }
