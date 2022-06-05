f4d88d7621efda3f5658ac953ec59ea870ed150b
tiagocarvalh0 <tiagocrv501@gmail.com>
2022-06-05 14:54:39 -0300

    up

--- 1º_SEMESTRE/L_PROGRAMACAO/atividades/list007/text06.c
+++ 1º_SEMESTRE/L_PROGRAMACAO/atividades/list007/text06.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 
-float sub_rot (int n){
-	float s = 1.0;
-	int i, j, fat = 1;
-	for (i = 1; i <= n; i++){
-		for (j = 1; j <= i; j++)
-			fat *= j;
-		s += (float) 1 / fat;
-		fat = 1;
-	}
-	return s;
-}
+int TAM = 5;
 
-int main() {
-	int numero;
+int main()
+{
+	void sun_Fun(void);
 	
-	printf("Digite um numero inteiro: ");
-	scanf("%d", &numero);
+	sun_Fun();
+
+	return 0;	
+}
+void sun_Fun(void)
+{
+	float vet[TAM], Maior, Menor;
+	int i;
 	
-	printf("%f\n", sub_rot(numero));
+	Maior = vet[0];
+	Menor = vet[0];
+	
+	for(i = 0; i < TAM; i++)
+	{
+		scanf("%f", &vet[i]);
+		if(vet[i] > Maior)
+			Maior = vet[i];
+		if(vet[i] < Menor)
+			Menor = vet[i];
+	}
+	
+	printf("Maior = %.1f\n", Maior);
+	printf("Menor = %.1f\n", Menor);
+
 }
--- 1º_SEMESTRE/L_PROGRAMACAO/atividades/list007/text08.c
+++ 1º_SEMESTRE/L_PROGRAMACAO/atividades/list007/text08.c
@@ -1,14 +1,90 @@
 #include <stdio.h>
 
+//int tam_Char = 7;
+//int tam_Idade = 5 ;
+
+
 int main()
 {
-	void sub_Fun(void);
+	/*char vet_1[tam_Char];
+	int vet_Idade[tam_Idade],i , j = 0;
+	
+	for(i = 1; i < tam_Char; i++)
+	{
+		scanf("%s", &vet_1[i]);
+		printf("Letra %c\n", vet_1[i]);
+		if(i % 3 == 0)
+		{
+			printf("Informe sua Idade:\n");
+			scanf("%d", &vet_Idade[j]);
+			printf("Idade %d\n", vet_Idade[j]);
+			j++;
+		}	
+	}*/
+	char vet_Mat[5][3];
+	int media_Idade = 0, cont = 0, soma;
+	int maior_Idade ;
+	int vet_Idade[6],linha, coluna, j = 0;
+	
+	for(linha = 1; linha < 6; linha++)
+	{
+		printf("========== Cadastro %d ==========\n", linha);
+		for(coluna = 1; coluna < 4; coluna++)
+		{
+			if(coluna == 1)
+			printf("Informe o Sexo:\n(M) =  Musculino\n(F) = Feminino\n");
+			if(coluna == 2)
+			printf("Informe a Cor dos Olho:\n(A) = Azul\n(C) = Castanho\n");
+			if(coluna == 3)
+			printf("Informe a Cor dos Cabelos:\n(L) = Louros\n(P) = Pretos\n(C) = Castanhos\n");
+			scanf("%s", &vet_Mat[linha][coluna]);
+			
+		}
+		printf("Informe a sua Idade:\n");
+		scanf("%d", &vet_Idade[linha]);
+	}
+	
+	for(linha = 1; linha < 6; linha++)
+		printf("%d ,", vet_Idade[linha]);
+		printf("\n");
+	
+	for(linha = 1; linha < 6; linha++)
+	{
+		for(coluna = 1; coluna < 4; coluna++)
+		{
+			printf("%c ", vet_Mat[linha][coluna]);
+		}
+		printf("\n");
+	}
+	
+	for(linha = 0; linha < 6; linha++)
+	{
+		if((vet_Mat[linha][2] == 'c') && (vet_Mat[linha][3] == 'p'))
+		{
+			printf("Numero da Variavel = %d\n", vet_Idade[linha]);
+			printf("Valor de Cont %d\n", cont);
+			soma = soma + vet_Idade[linha];
+			printf("Soma = %d\n", soma);
+			cont++;
+		}
+		linha++;
+	}
 
+	for(linha = 1; linha < 6; linha++)
+		printf("%d ,", vet_Idade[linha]);
+		printf("\n");
+	maior_Idade = vet_Idade[1];
+	for(linha = 1; linha < 6; linha++)
+		if(vet_Idade[linha] > maior_Idade)
+			maior_Idade = vet_Idade[linha];
+	printf("Maior Idade = %d\n", maior_Idade);
+	
+	media_Idade = soma / cont;
+	printf("Media Idade (C)(P) = %d\n", media_Idade);
+	
+	
+	
+		
 
-}
-void sub_Fun(void)
-{
-
-
-
+		
 }
