b65fb5679b9b4acebd951963e234a9fa68fd5cd5
tiagocarvalh0 <tiagocrv501@gmail.com>
2022-06-10 20:20:42 -0300

    up

---
+++ 1º_SEMESTRE/L_PROGRAMACAO/atividades/list008/text01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct 
+{
+	int cadastro_Contato ;
+	int localizar_Contato ;
+	int alterar_Contato ;
+	int apagar_Contato ;
+	int listar_Contato ;
+	int media_Idade ;
+} menu ;
+
+int main ()
+{
+	printf("ola\n");
+
+	return 0;
+}
+
