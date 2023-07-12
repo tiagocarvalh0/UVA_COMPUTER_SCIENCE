bf9083f354cb9ab0f896e4004adfec95d52be6dc
tiagocarvalh0 <tiagocrv501@gmail.com>
2023-07-12 14:51:26 -0300

    up

--- APOSTA/APOSTA/Aposta.h
+++
@@ -1,4 +0,0 @@
-
-void telaMenu();
-void menuInicial();
-
--- p.py
+++
@@ -1,5 +0,0 @@
-import pyexcel as pe
-sheet = pe.get_sheet(file_name="example.xls")
-sheet.row += [12, 11, 10]
-sheet.save_as("new_example.xls")
-pe.get_sheet(file_name="new_example.xls")
